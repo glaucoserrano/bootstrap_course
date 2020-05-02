2bd6196b03c56cb73c95004b2e3d22bbf1fab6ca
glaucoserrano <51676475+glaucoserrano@users.noreply.github.com>
2020-05-02 11:35:17 -0300

    Site Pronto

--- js/scripts.js
+++ js/scripts.js
@@ -134,5 +134,38 @@ $(document).ready(function(){
             });
         }
     }
+    //Scroll para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection= $('#mainSlider');
+    let aboutSection =$('#about-area');
+    let serviceSection=$('#services-area');
+    let teamSection=$('#team-area');
+    let portfolioSection=$('#portfolio-area');
+    let contactSection=$('#contact-area');
+    
+    let scrollTo = '0';
+    $(navBtn).click(function() {
+        let btnId = $(this).attr('id');
+
+        if(btnId=='about-menu'){
+            scrollTo=aboutSection;
+        }else if (btnId=='services-menu'){
+            scrollTo=serviceSection;
+        }else if(btnId=='team-menu'){
+            scrollTo=teamSection;
+        }else if(btnId=='portifolio-menu'){
+            scrollTo=portfolioSection;
+        }else if(btnId=='contact-menu'){
+            scrollTo=contactSection;
+        }else{
+            scrollTo=bannerSection;
+        }
+        console.log(scrollTo);
+
+        $([document.documentElement,document.body]).animate({
+            scrollTop: $(scrollTo).offset().top -70
+        },1500);
+    });
 
 });
